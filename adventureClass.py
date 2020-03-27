0a48d2e89d86da86c2bd7d9c63b0c2b861113ebf
Bradinator64 <bradaz64@gmail.com>
2020-03-27 08:40:17 +0000

    Eddited AdventureClass so that the user only has to enter a list of choices instead of a dictionary.  The dictionary is generated from the inputted list.

--- adventureClass.py
+++ adventureClass.py
@@ -1,7 +1,15 @@
 class AdventureClass: #Prints out text and displays choices
-    def __init__(self, text, choices=None):
+    def __init__(self, text, choices_input=[]):
         self.text = text
-        self.choices = choices
+        self.choices_input = choices_input
+        self.choices_array = range(len(choices_input)) #This array will be used to assign the values from self.choices_input to their repective keys in the self.choice dictionary.
+        self.choices = {} #This will be used to store all the choices available to the user.
+        for choice in self.choices_array: #Assigning each choice to it's repective key in the dictionary.
+            key = self.choices_array[choice]+1 #Correction, else choice 1 will have a key of 0.
+            value = self.choices_input[choice]
+            self.choices[key] = value
+            print("Adding {value} to {key}".format(value=value, key=key))
+        
 
     def printAdventure(self):
         printedText = self.text + "\n\n" #Assigns the description text to a variable.
@@ -11,7 +19,7 @@ class AdventureClass: #Prints out text and displays choices
         print(printedText) #returns the variable.
 
 testPara = AdventureClass("""
-This is a test case""", {1: "choice 1", 2: "choice 2"})
+This is a test case""", ["choice 1", "choice 2"])
 testPara2 = AdventureClass("""
 This is also a test case""")
 
